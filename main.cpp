be25f20dce161e2966cd512e8edfb3f5b2751bc7
mhosein <mhmtsze@gmail.com>
2026-04-10 12:31:03 +0330

    remove(main): remove test

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 int main() {
 
     Router router;
-    std::unordered_map<std::string, std::string> test;
-    test["test"] = "test";
-    router.find_route("user_profile", GET, test);
+
     HTTPServer server;
     server.Loop();
 
