bd34a952239549b1bb32a4c84f4a6e3757fd7acf
mhosein <mhmtsze@gmail.com>
2026-05-05 09:50:42 +0330

    feat(main)

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 
 #include "src/core/Core.h"
 #include "src/core/Log.h"
+#include "src/lua/LuaBinding.h"
 #include "src/routing/Router.h"
 
 int main() {
 
     Router router;
 
+    Lua::getInstance().Initialize();
+    Lua::getInstance().LoadScripts();
+
     const HTTPServer server;
-    server.Loop();
+    server.Loop(&router);
 
     return 0;
 }
